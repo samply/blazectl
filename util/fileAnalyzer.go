93fa3e75cf37f5d5bbbdc5db21b5acff99c5449d
Alexander Twrdik <atwrdik@life.uni-leipzig.de>
2020-11-23 18:27:09 +0100

    Allow Uploads of NDJSON Files
    Requires the files in question to have an
    appropriate file extension (.ndjson)

---
+++ util/fileAnalyzer.go
@@ -0,0 +1,84 @@
+package util
+
+import (
+	"io"
+)
+
+// Size of the buffer used for calculating file chunks.
+const chunksCalculationBufferSizeBytes = 4096
+
+// FileChunk describes a chunk within a file with its starting position and end
+// position in bytes. Both are given as bytes counted from the file's beginning.
+// Also carries information about the chunk number (i.e. its order position)
+// within the file.
+type FileChunk struct {
+	ChunkNumber int
+	StartBytes  int64
+	EndBytes    int64
+}
+
+// FileChunkCalculationResult represents a single result of a file chunk calculation.
+// Carries information about a FileChunk and additional error information.
+// A FileChunk can still hold valuable information even in the presence of an
+// error such as the chunk number.
+type FileChunkCalculationResult struct {
+	FileChunk FileChunk
+	Err       error
+}
+
+// CalculateFileChunks calculates all chunks of r that are delimited by delimiter.
+// r is read in a streamed fashion.
+// Results will be published on a res channel as they appear when reading r.
+// Closes the result channel as soon as r is exhaustively read.
+func CalculateFileChunks(r io.Reader, delimiter byte, res chan<- FileChunkCalculationResult) {
+	var lastSeenDelimiterTokenOffsetBytes int64 = 0
+	alreadyReadBytes := int64(0)
+	chunkNumber := 0
+	buf := make([]byte, 0, chunksCalculationBufferSizeBytes)
+	for {
+		n, err := r.Read(buf[:cap(buf)])
+		buf = buf[:n]
+		if n == 0 {
+			if err == nil {
+				continue
+			}
+			if err == io.EOF {
+				// For when r does not end with the delimiter.
+				if alreadyReadBytes > lastSeenDelimiterTokenOffsetBytes {
+					res <- FileChunkCalculationResult{
+						FileChunk: FileChunk{
+							ChunkNumber: chunkNumber + 1,
+							StartBytes:  lastSeenDelimiterTokenOffsetBytes,
+							EndBytes:    alreadyReadBytes,
+						},
+					}
+				}
+
+				close(res)
+				break
+			}
+			res <- FileChunkCalculationResult{
+				FileChunk: FileChunk{
+					ChunkNumber: chunkNumber,
+				},
+				Err: err,
+			}
+		}
+
+		for idx, b := range buf {
+			if b == delimiter {
+				chunkNumber++
+				res <- FileChunkCalculationResult{
+					FileChunk: FileChunk{
+						ChunkNumber: chunkNumber,
+						StartBytes:  lastSeenDelimiterTokenOffsetBytes,
+						EndBytes:    alreadyReadBytes + int64(idx),
+					},
+				}
+				lastSeenDelimiterTokenOffsetBytes = alreadyReadBytes + int64(idx) + 1
+			}
+		}
+
+		alreadyReadBytes += int64(n)
+	}
+}
