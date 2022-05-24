cbd3d45b215d9beb4b9ea88181b511450b78b219
Alexander Kiel <alexanderkiel@gmx.net>
2022-05-24 21:21:14 +0200

    Release v0.9.0

--- build-releases.sh
+++ build-releases.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION=0.8.6
+VERSION=0.9.0
 
 mkdir -p builds
 
--- cmd/root.go
+++ cmd/root.go
@@ -52,7 +52,7 @@ var rootCmd = &cobra.Command{
 	Long: `blazectl is a command line tool to control your FHIR® server.
 
 Currently you can upload transaction bundles from a directory, download and count resources.`,
-	Version: "0.8.6",
+	Version: "0.9.0",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
